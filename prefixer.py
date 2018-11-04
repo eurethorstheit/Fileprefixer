10da28722b70082e123ed3da6b98555301dce264
shrick <sickhickrick@women-at-work.org>
2018-11-04 02:27:52 +0100

    Rename logic put into separate class

--- prefixer.py
+++ prefixer.py
@@ -8,14 +8,7 @@ from operator import itemgetter
 
 '''
 Last Changes:
-    - Linter-Warnungen behoben
-    - Kein globaler Code
-    - Nicht verwendeten Code entfernt
-    - Einlesen sortierter Dateiliste
-    - Regex gehärtet
-    - Widgets root-Windows zugeordnet, Dateiliste Fenster-füllend
-    - Sortierung/Umbenennung vereinfacht
-    - Dateiliste wird nach Umbenennung aktualisiert
+    - Rename-Logik in separate Klasse ausgelagert
 
 To do:
     Filter setzen
@@ -29,22 +22,61 @@ Bugs:
 '''
 
 
+class FileRenamer:
+    def __init__(self, directory, suffix):
+        self.directory = directory
+        self.suffix = suffix
+        self.filter = "*" + self.suffix
+        self.regex = (r"^(\d+_){0,1}(.+" + re.escape(suffix) + r")$")
+        self.items = []
+
+    def get_directory(self):
+        return self.directory
+
+    def get_suffix(self):
+        return self.suffix
+
+    def refresh(self):
+        self.items = []
+
+        for item in sorted(os.listdir(self.directory)):
+            if fnmatch.fnmatch(item, self.filter):
+                match = re.search(self.regex, item)
+                if match:
+                    # (orig, base, number or None)
+                    self.items.append((item, match.group(2), match.group(1)))
+
+        return [i[0] for i in self.items]
+
+    def rename(self):
+        sorted_list = (
+            # sort numbered items by existimg number
+            sorted((i for i in self.items if i[2]),
+                   key=itemgetter(2)) +
+            # sort unnumbered items by name
+            sorted((i for i in self.items if not i[2]),
+                   key=itemgetter(1))
+        )
+
+        for number, (orig, base, _) in enumerate(sorted_list, 1):
+            renamed = "{0:03d}_{1}".format(number, base)
+            if orig == renamed:
+                print("renaming", orig, "skipped")
+            else:
+                print("renaming", orig, "->", renamed)
+                os.rename(
+                    os.path.join(self.directory, orig),
+                    os.path.join(self.directory, renamed))
+
+
 class App(tk.Frame):
     ''' Die Hauptklasse '''
 
-    def __init__(self, config, master=None):
-        super().__init__(master)
-        self.config = config
-        self.pack()
-        # [Ordner, Dateierweiterung]
-        self.setting = [
-            self.config['DEFAULT']['folder'],
-            "*" + self.config['DEFAULT']['filter']]
-        self.regex = (
-            r"^(\d+_){0,1}(.+" +
-            re.escape(self.config['DEFAULT']['filter']) +
-            r")$")
+    def __init__(self, renamer, master=None):
+        self.renamer = renamer
 
+        super().__init__(master)
+        self.pack()
         self.create_widgets()
         self.refresh_list_unsorted()
 
@@ -52,11 +84,11 @@ class App(tk.Frame):
         ''' Erstellt die Widgets '''
 
         self.l_folder = tk.Label(self.master)
-        self.l_folder["text"] = "Dateiordner:", self.config['DEFAULT']['folder']
+        self.l_folder["text"] = "Dateiordner:", self.renamer.get_directory()
         self.l_folder.pack(side="top")
 
         self.l_filter = tk.Label(self.master)
-        self.l_filter["text"] = "Filter:", self.config['DEFAULT']['filter']
+        self.l_filter["text"] = "Filter:", self.renamer.get_suffix()
         self.l_filter.pack(side="top")
 
         self.list_unsorted = tk.Listbox(self.master)
@@ -75,45 +107,13 @@ class App(tk.Frame):
         self.b_quit = tk.Button(self.master, text="Quit", command=root.destroy)
         self.b_quit.pack(side="bottom")
 
-    def get_items(self):
-        ''' Holt die Items in einem Angegebenen Ordner (Folder) '''
-        filtered = []
-
-        for item in sorted(os.listdir(self.setting[0])):
-            if fnmatch.fnmatch(item, self.setting[1]):
-                match = re.search(self.regex, item)
-                if match:
-                    # (orig, base, number or None)
-                    filtered.append((item, match.group(2), match.group(1)))
-
-        return filtered
-
     def refresh_list_unsorted(self):
         ''' aktualisiert den Inhalt des Ordners '''
-        self.items = self.get_items()
-
         self.list_unsorted.delete(0, tk.END)
-        self.list_unsorted.insert(tk.END, *[i[0] for i in self.items])
+        self.list_unsorted.insert(tk.END, *self.renamer.refresh())
 
     def sort_start(self):
-        sorted_list = (
-            # sort numbered items by existimg number
-            sorted((i for i in self.items if i[2]),
-                   key=itemgetter(2)) +
-            # sort unnumbered items by name
-            sorted((i for i in self.items if not i[2]),
-                   key=itemgetter(1))
-        )
-
-        for number, (orig, base, _) in enumerate(sorted_list, 1):
-            renamed = "{0:03d}_{1}".format(number, base)
-            if orig == renamed:
-                print(orig, "skipped")
-            else:
-                print(orig, "->", renamed)
-                os.rename(
-                    os.path.join(self.setting[0], orig),
-                    os.path.join(self.setting[0], renamed))
+        self.renamer.rename()
         self.refresh_list_unsorted()
 
 
@@ -124,11 +124,15 @@ if __name__ == "__main__":
 
     # set fallback configuration
     config.setdefault('DEFAULT', {
-        'Folder': './Test/',
-        'Filter': '.txt'})
+        'folder': './Test/',
+        'filter': '.txt'})
+
+    filerenamer = FileRenamer(
+        config['DEFAULT']['folder'],
+        config['DEFAULT']['filter'])
 
     root = tk.Tk()
-    app = App(master=root, config=config)
+    app = App(master=root, renamer=filerenamer)
     app.mainloop()
 
     with open('settings.ini', 'w') as configfile:
